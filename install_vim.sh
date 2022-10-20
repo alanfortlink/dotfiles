aacba61babcc36fa9a8c781320ed1aa205650ac9
Alan M. Silva <oalanms@gmail.com>
2022-10-21 00:44:38 +0100

    Update

--- install_vim.sh
+++ install_vim.sh
@@ -13,4 +13,3 @@ PACKER_INSTALL_DIR="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 COPILOT_INSTALL_DIR="$HOME/.config/nvim/pack/github/start/copilot.vim"
 
 [[ ! -d $PACKER_INSTALL_DIR ]] && git clone --depth 1 https://github.com/wbthomason/packer.nvim $PACKER_INSTALL_DIR
-[[ ! -d $COPILOT_INSTALL_DIR ]] && git clone https://github.com/github/copilot.vim.git $COPILOT_INSTALL_DIR
