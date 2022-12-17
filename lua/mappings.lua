f764ec3a26c87a1bfd018ce7be20c8513896b8c2
Alan Silva <alanfortlink@gmail.com>
2022-12-17 17:51:43 +0000

    Update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -46,8 +46,13 @@ local telescope = require('telescope.builtin')
 
 -- Nice find files
 vim.keymap.set('n', '<leader>f', function() telescope.find_files({ hidden = false }) end)
+
 -- Messy find files
 vim.keymap.set('n', '<leader>F', function() telescope.find_files({ hidden = true }) end)
+
+-- Commands
+vim.keymap.set('n', '<leader>c', telescope.commands)
+vim.keymap.set('n', '<leader>h', telescope.help_tags)
 
 -- Almighty GREP
 vim.keymap.set('n', '<leader>g', telescope.live_grep)
