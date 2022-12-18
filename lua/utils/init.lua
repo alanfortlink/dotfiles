9b4e0305bcf3bc3ffad4e2b0e4be1ec806ce5c10
Alan Silva <alanfortlink@gmail.com>
2022-12-18 14:25:22 +0000

    Update

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -4,22 +4,6 @@ end
 
 local M = {}
 
-local function set_keymap(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { noremap = true })
-end
-
-function M.nmap(lhs, rhs)
-    set_keymap("n", lhs, rhs)
-end
-
-function M.vmap(lhs, rhs)
-    set_keymap("v", lhs, rhs)
-end
-
-function M.imap(lhs, rhs)
-    set_keymap("i", lhs, rhs)
-end
-
 function M.get_quickfix_bufnr()
     local all_buffers = vim.fn.getbufinfo()
     for _, buffer in ipairs(all_buffers) do
