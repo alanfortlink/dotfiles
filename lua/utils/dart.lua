5a91a0bb207e6ab31708ea68f9cb5253cc030679
Alan Silva <alanfortlink@gmail.com>
2023-01-15 17:16:01 +0000

    Update

--- lua/utils/dart.lua
+++ lua/utils/dart.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         vim.keymap.set("n", "<F4>", ":FlutterDevices<CR>")
         vim.keymap.set("n", "<F5>", ":FlutterRun<CR>")
-        vim.keymap.set("n", "<F6>", ":FlutterVSplit<CR>")
+        vim.keymap.set("n", "<F6>", ":FlutterDevTools<CR>")
         vim.keymap.set("n", "<F7>", ":FlutterHotRestart<CR>")
         vim.keymap.set("n", "<F8>", ":FlutterQuit<CR>")
     end,
