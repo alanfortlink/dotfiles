555fcbc85f4d315e436a7c5b507578575f0dc8ac
Alan Silva <alanfortlink@gmail.com>
2023-01-15 22:15:55 +0000

    Update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,6 +4,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<localleader>a", mark.add_file)
+vim.keymap.set("n", "<localleader>r", mark.rm_file)
 vim.keymap.set("n", "<localleader>A", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<localleader>n", ui.nav_next)
