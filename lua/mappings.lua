a14cbb1095d7925a7ed4cf4361d4179eb3456caf
Alan Silva <alanfortlink@gmail.com>
2023-01-15 17:35:13 +0000

    Update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,10 @@
 local utils = require("utils")
 
 -- Window movement
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h", {noremap = true})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {noremap = true})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {noremap = true})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {noremap = true})
 
 -- Window resizing
 vim.keymap.set("n", "<C-M-h>", "3<C-w><")
