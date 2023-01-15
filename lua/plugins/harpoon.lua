7bc5a0a062e344b2ce2b06a3030588f6ed16335b
Alan Silva <alanfortlink@gmail.com>
2023-01-15 22:23:48 +0000

    Update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<localleader>a", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<localleader>n", ui.nav_next)
 vim.keymap.set("n", "<localleader>p", ui.nav_prev)
+
+for i = 1, 9 do vim.keymap.set("n", "<localleader>" .. i, function() ui.nav_file(i) end) end
