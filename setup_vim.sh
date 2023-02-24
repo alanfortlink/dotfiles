13823c875956fc5816ad63477661a87ffb57ea6c
Alan Silva <alanfortlink@gmail.com>
2023-02-24 21:45:07 +0000

    Update

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 NVIM_HOME="$HOME/.config/nvim"
-NVIM_LUA_HOME="$NVIM_HOME/lua"
 
-mkdir -p $NVIM_LUA_HOME
+rm -rf $HOME/.old.nvim
+mv $NVIM_HOME $HOME/.old.nvim
+mkdir -p $NVIM_HOME
 
-cp init.lua $NVIM_HOME
 cp -R snippets $NVIM_HOME
-cp -R lua/* $NVIM_LUA_HOME
+cp -R plugin $NVIM_HOME
+cp -R lua $NVIM_HOME
 
 PACKER_INSTALL_DIR="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 [[ ! -d $PACKER_INSTALL_DIR ]] && git clone --depth 1 https://github.com/wbthomason/packer.nvim $PACKER_INSTALL_DIR
