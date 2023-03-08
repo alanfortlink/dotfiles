8b2af26ad1107779492a063c41a6102589dde3da
Alan Silva <alanfortlink@gmail.com>
2023-03-08 20:27:11 +0000

    Update

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,4 +1,3 @@
 -- Undo tree
 vim.keymap.set("n", "<localleader>u", ":UndotreeShow<CR>:UndotreeFocus<CR>", { noremap = true})
 vim.keymap.set("n", "<localleader>U", ":UndotreeHide<CR>", { noremap = true})
-
