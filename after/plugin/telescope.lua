fd54f0ba33ba7cb17a7f0de5cc2b4e8560f4d366
Alan Silva <alanfortlink@gmail.com>
2023-03-22 22:01:05 +0000

    Update

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -26,7 +26,8 @@ vim.keymap.set("n", "<localleader>c", telescope.commands, { noremap = true })
 vim.keymap.set("n", "<localleader>h", telescope.help_tags, { noremap = true })
 
 -- Almighty GREP
-vim.keymap.set("n", "<leader>g", telescope.live_grep, { noremap = true })
+vim.keymap.set("n", "<leader>g", telescope.grep_string, { noremap = true })
+vim.keymap.set("v", "<leader>g", telescope.grep_string, { noremap = true })
 
 -- Other telescope stuff
 vim.keymap.set("n", "<localleader>d", telescope.lsp_definitions, { noremap = true })
