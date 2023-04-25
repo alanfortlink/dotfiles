c2a9b2f4fc87b738e8095447d1f8d7ab73eecc9f
Alan Silva <alanfortlink@gmail.com>
2023-04-25 21:45:54 +0100

    Update

--- lua/config/sets.lua
+++ lua/config/sets.lua
@@ -42,7 +42,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 vim.opt.background = "dark"
-if not pcall(function() vim.cmd.colorscheme("terafox") end) then
+if not pcall(function() vim.cmd.colorscheme("codedark") end) then
   print("carbonfox not found!")
 end
 
