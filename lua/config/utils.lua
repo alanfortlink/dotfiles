e0c5cc8a07d241713219c2de27a5fd195e1a32cb
Alan Silva <asilva173@bloomberg.net>
2023-05-31 06:44:01 -0400

    Update

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -21,18 +21,35 @@ local function exec(command)
     return table[#table-1]
 end
 
-function M.get_github_link()
-    -- github.com/<organization>/<repository>/blob/<branch_name>/<path_to_file>?plain=1
-    local url = exec("!git config --get remote.origin.url | sed \"s/\\.git$//\" | sed \"s/^git@/https:\\/\\//\"")
-    local branch = exec("!git branch --show-current")
+local parse_git_url = function()
+    package.preload["config.utils"] = nil
+    package.loaded["config.utils"] = nil
+    local url = exec("!git config --get remote.origin.url")
 
+    local _ = nil
+    local domain = nil
+    local org = nil
+    local repo = nil
+
+    if url.find(url, "^git@") ~= nil then
+        _, _, domain, org, repo = string.find(url, "git@(.*):(.*)/(.*)")
+    else
+        _, _, domain, org, repo = string.find(url, "http.*://(.*)/(.*)/(.*)")
+    end
+
+    return domain, org, repo
+end
+
+M.get_github_link = function()
+    local domain, org, repo = parse_git_url()
+    local branch = exec("!git branch --show-current")
     local file_path = vim.api.nvim_buf_get_name(0)
     local cwd = vim.fn.getcwd()
 
     local r_path = file_path:gsub(cwd .. "/", "")
     local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-    local link = url .. "/blob/" .. branch .. "/" .. r_path .. "#L" .. row
+    local link = string.format("https://%s/%s/%s/blob/%s/%s#L%s", domain, org, repo, branch, r_path, row)
     return link
 end
 
