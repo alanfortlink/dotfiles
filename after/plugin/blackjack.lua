8a8f86a6e99ecad130a4f9c4825069b4ae459f93
Alan Silva <alanfortlink@gmail.com>
2023-09-23 09:34:17 +0200

    Update blackjack usage

--- after/plugin/blackjack.lua
+++ after/plugin/blackjack.lua
@@ -1,7 +1,11 @@
 require("blackjack").setup({
   card_style = "small",
   suit_style = "white",
-  scores_path = "$HOME/.blackjack_scores.json",
+  keybindings = {
+    ["next"] = "8",
+    ["finish"] = "9",
+    ["quit"] = "0",
+  }
 })
 
 vim.keymap.set("n", "<localleader>pb", ":BlackJackNewGame<CR>", { noremap = true })
