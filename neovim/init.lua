bd5f6d793069ab2baf077849f5a0a230ea28ad53
Alan Silva <alanfortlink@gmail.com>
2024-01-24 21:00:49 +0000

    Update

--- neovim/init.lua
+++ neovim/init.lua
@@ -269,8 +269,30 @@ require("lazy").setup({
     "williamboman/mason.nvim",
     lazy = false,
     config = function() 
-      require("mason").setup()
     end,
+    dependencies = {
+      { 
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function() 
+          require("mason").setup()
+          require("mason-lspconfig").setup()
+          require("mason-lspconfig").setup_handlers {
+              -- The first entry (without a key) will be the default handler
+              -- and will be called for each installed server that doesn't have
+              -- a dedicated handler.
+              function (server_name) -- default handler (optional)
+                  require("lspconfig")[server_name].setup {}
+              end,
+              -- Next, you can provide a dedicated handler for specific servers.
+              -- For example, a handler override for the `rust_analyzer`:
+              -- ["rust_analyzer"] = function ()
+              --     require("rust-tools").setup {}
+              -- end
+          }
+        end,
+      },
+    },
   },
   { 
     "akinsho/flutter-tools.nvim", 
@@ -312,5 +334,4 @@ require("lazy").setup({
     'github/copilot.vim',
     config = function() end,
   },
-  { "williamboman/mason-lspconfig.nvim" },
 })
