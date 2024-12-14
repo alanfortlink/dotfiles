90c392cc41c9d6c341e4e9867e2d30cf6bf84b7f
Alan Silva <alanfortlink@gmail.com>
2024-12-14 23:58:03 +0000

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -49,8 +49,8 @@ require('lazy').setup({
 
           vim.keymap.set("n", "<leader><leader>", buf.format, { noremap = true })
 
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true })
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true })
+          vim.keymap.set("n", "]e", vim.diagnostic.goto_next, { noremap = true })
+          vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, { noremap = true })
           vim.keymap.set("n", "ge", vim.diagnostic.open_float, { noremap = true })
 
           vim.keymap.set("n", "gwc", utils.code_action_wrapper("container", buf), { noremap = true })
