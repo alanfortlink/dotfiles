5ff52e00d05fb2e07f805ba656b588a6cea81d1e
Alan Silva <alanfortlink@gmail.com>
2024-12-15 03:10:04 +0000

    Update

--- nvim/lua/config/telescope/multigrep.lua
+++ nvim/lua/config/telescope/multigrep.lua
@@ -58,60 +58,6 @@ local live_multigrep = function(opts)
   }):find();
 end
 
-local file_multigrep = function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd or vim.uv.cwd()
-
-  local finder = finders.new_async_job {
-    command_generator = function(prompt)
-      if not prompt or prompt == "" then
-        return nil
-      end
-
-      local parts = vim.split(prompt, "  ")
-      local args = { "rg" }
-
-      if parts[1] then
-        table.insert(args, "-g")
-        table.insert(args, parts[1])
-      end
-
-      local i = 2
-      while parts[i] do
-        if parts[i] then
-          table.insert(args, "-e")
-          table.insert(args, parts[i])
-        end
-        i = i + 1
-      end
-
-      ---@diagnostic disable-next-line: deprecated
-      return vim.tbl_flatten({
-        args,
-        {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-        },
-      })
-    end,
-    entry_maker = make_entry.gen_from_vimgrep(opts),
-    cwd = opts.cwd,
-  }
-
-  pickers.new(opts, {
-    debounce = 100,
-    prompt = "Multi Grep",
-    finder = finder,
-    previewer = conf.grep_previewer(opts),
-    sorter = require("telescope.sorters").empty()
-  }):find();
-end
-
 M.live_multigrep = live_multigrep;
-M.file_multigrep = file_multigrep;
 
 return M
