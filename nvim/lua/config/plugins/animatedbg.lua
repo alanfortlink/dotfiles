f278a1e256ea67d1e09754a56c69c46838b63202
Alan Silva <alanfortlink@gmail.com>
2024-12-26 22:30:25 +0000

    Update

--- nvim/lua/config/plugins/animatedbg.lua
+++ nvim/lua/config/plugins/animatedbg.lua
@@ -5,3 +5,12 @@ return {
     require("animatedbg-nvim").setup({})
   end
 }
+
+--return {
+--  'alanfortlink/animatedbg.nvim',
+--  config = function()
+--    require("animatedbg-nvim").setup({
+--      fps = 60   -- default
+--    })
+--  end
+--}
