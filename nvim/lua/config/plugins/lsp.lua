0426ce92d19328c251733a55272ea19b97e984d2
Alan Silva <alanfortlink@gmail.com>
2024-12-29 01:17:26 +0000

    make it linux

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -84,7 +84,7 @@ return {
       require('lspconfig').clangd.setup({
         capabilities = capabilities,
         cmd = {
-          '/home/alan/apps/llvm/LLVM-19.1.0-Linux-X64/bin/clangd'
+          '/home/alan/tools/llvm/LLVM-19.1.0-Linux-X64/bin/clangd'
           -- '/opt/homebrew/Cellar/llvm/19.1.4/bin/clangd',
         }
       })
