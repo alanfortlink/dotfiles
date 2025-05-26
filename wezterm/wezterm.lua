5db0804806dbcfa418119f87e6d1f3967bf8a9d2
Alan Silva <alanfortlink@gmail.com>
2025-05-26 22:48:04 +0100

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,6 @@
 local wezterm = require 'wezterm'
 
-local session_manager = require 'wezterm-session-manager.session-manager'
-
-wezterm.on("save_session", function(window) session_manager.save_state(window) end)
-wezterm.on("load_session", function(window) session_manager.load_state(window) end)
-wezterm.on("restore_session", function(window) session_manager.restore_state(window) end)
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 
 return {
   font = wezterm.font('Pragmasevka Nerd Font'), -- Replace with your desired font family
@@ -38,30 +34,64 @@ return {
     { key = "v", mods = "OPT",        action = wezterm.action.SplitHorizontal },
     { key = "q", mods = "OPT",        action = wezterm.action.CloseCurrentPane({ confirm = false }) },
 
-    { key = "w", mods = "OPT",        action = wezterm.action{EmitEvent = "save_session"} },
-    { key = "r", mods = "OPT",        action = wezterm.action{EmitEvent = "restore_session"} },
+    {
+      key = "w",
+      mods = "ALT",
+      action = wezterm.action_callback(function(win, pane)
+        resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+      end),
+    },
+    -- { key = "w", mods = "OPT",        action = wezterm.action{EmitEvent = "save_session"} },
+    -- { key = "r", mods = "OPT",        action = wezterm.action{EmitEvent = "restore_session"} },
 
-    { key = "h", mods = "OPT",        action = wezterm.action.ActivatePaneDirection "Left" },
-    { key = "j", mods = "OPT",        action = wezterm.action.ActivatePaneDirection "Down" },
-    { key = "k", mods = "OPT",        action = wezterm.action.ActivatePaneDirection "Up" },
-    { key = "l", mods = "OPT",        action = wezterm.action.ActivatePaneDirection "Right" },
-    { key = "z", mods = "OPT",        action = wezterm.action.TogglePaneZoomState },
+    {
+      key = "r",
+      mods = "ALT",
+      action = wezterm.action_callback(function(win, pane)
+        resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+          local type = string.match(id, "^([^/]+)") -- match before '/'
+          id = string.match(id, "([^/]+)$")       -- match after '/'
+          id = string.match(id, "(.+)%..+$")      -- remove file extention
+          local opts = {
+            relative = true,
+            restore_text = true,
+            on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+          }
+          if type == "workspace" then
+            local state = resurrect.state_manager.load_state(id, "workspace")
+            resurrect.workspace_state.restore_workspace(state, opts)
+          elseif type == "window" then
+            local state = resurrect.state_manager.load_state(id, "window")
+            resurrect.window_state.restore_window(pane:window(), state, opts)
+          elseif type == "tab" then
+            local state = resurrect.state_manager.load_state(id, "tab")
+            resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+          end
+        end)
+      end),
+    },
 
-    { key = "h", mods = "OPT|SHIFT",  action = wezterm.action.AdjustPaneSize { "Left", 5 } },
-    { key = "j", mods = "OPT|SHIFT",  action = wezterm.action.AdjustPaneSize { "Down", 5 } },
-    { key = "k", mods = "OPT|SHIFT",  action = wezterm.action.AdjustPaneSize { "Up", 5 } },
-    { key = "l", mods = "OPT|SHIFT",  action = wezterm.action.AdjustPaneSize { "Right", 5 } },
+    { key = "h", mods = "OPT",       action = wezterm.action.ActivatePaneDirection "Left" },
+    { key = "j", mods = "OPT",       action = wezterm.action.ActivatePaneDirection "Down" },
+    { key = "k", mods = "OPT",       action = wezterm.action.ActivatePaneDirection "Up" },
+    { key = "l", mods = "OPT",       action = wezterm.action.ActivatePaneDirection "Right" },
+    { key = "z", mods = "OPT",       action = wezterm.action.TogglePaneZoomState },
 
-    { key = "c", mods = "OPT",        action = wezterm.action.SpawnTab "CurrentPaneDomain" },
-    { key = "1", mods = "OPT",        action = wezterm.action.ActivateTab(0) },
-    { key = "2", mods = "OPT",        action = wezterm.action.ActivateTab(1) },
-    { key = "3", mods = "OPT",        action = wezterm.action.ActivateTab(2) },
-    { key = "4", mods = "OPT",        action = wezterm.action.ActivateTab(3) },
-    { key = "5", mods = "OPT",        action = wezterm.action.ActivateTab(4) },
-    { key = "6", mods = "OPT",        action = wezterm.action.ActivateTab(5) },
-    { key = "7", mods = "OPT",        action = wezterm.action.ActivateTab(6) },
-    { key = "8", mods = "OPT",        action = wezterm.action.ActivateTab(7) },
-    { key = "9", mods = "OPT",        action = wezterm.action.ActivateTab(8) },
+    { key = "h", mods = "OPT|SHIFT", action = wezterm.action.AdjustPaneSize { "Left", 5 } },
+    { key = "j", mods = "OPT|SHIFT", action = wezterm.action.AdjustPaneSize { "Down", 5 } },
+    { key = "k", mods = "OPT|SHIFT", action = wezterm.action.AdjustPaneSize { "Up", 5 } },
+    { key = "l", mods = "OPT|SHIFT", action = wezterm.action.AdjustPaneSize { "Right", 5 } },
+
+    { key = "c", mods = "OPT",       action = wezterm.action.SpawnTab "CurrentPaneDomain" },
+    { key = "1", mods = "OPT",       action = wezterm.action.ActivateTab(0) },
+    { key = "2", mods = "OPT",       action = wezterm.action.ActivateTab(1) },
+    { key = "3", mods = "OPT",       action = wezterm.action.ActivateTab(2) },
+    { key = "4", mods = "OPT",       action = wezterm.action.ActivateTab(3) },
+    { key = "5", mods = "OPT",       action = wezterm.action.ActivateTab(4) },
+    { key = "6", mods = "OPT",       action = wezterm.action.ActivateTab(5) },
+    { key = "7", mods = "OPT",       action = wezterm.action.ActivateTab(6) },
+    { key = "8", mods = "OPT",       action = wezterm.action.ActivateTab(7) },
+    { key = "9", mods = "OPT",       action = wezterm.action.ActivateTab(8) },
 
     {
       key = "u",
