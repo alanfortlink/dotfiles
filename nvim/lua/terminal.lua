086cdb836b88d9940c6dcf3de6124b2933d404f3
Alan Silva <alanfortlink@gmail.com>
2025-06-15 16:36:49 +0100

    update

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -8,7 +8,7 @@ local state = {
 local function create_floating_window(opts)
   opts = opts or {}
   local width = opts.width or math.floor(vim.o.columns * 0.90)
-  local height = opts.height or math.floor(vim.o.lines * 0.90)
+  local height = opts.height or math.floor(vim.o.lines * 0.80)
 
   -- Calculate the position to center the window
   local col = math.floor((vim.o.columns - width) / 2)
